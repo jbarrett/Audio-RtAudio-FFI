616d5eaf9bcc7a9991e5316c7a0bbc5b3cb7c331
John Barrett <john@jbrt.org>
2024-07-05 09:22:23 +0100

    LFO implementation

--- examples/lfo.pl
+++ examples/lfo.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/env perl
 
+# Create a sine LFO with a period of 1 second
+
 use v5.40;
 
 use Audio::RtAudio::FFI ':all';
+use Math::Trig ':pi';
+use FFI::Platypus::Buffer qw/ scalar_to_buffer /;
+use FFI::Platypus::Memory qw/ memcpy free /;
 
 my $samplerate = 48_000;
 my $bufsize = 256;
@@ -19,8 +24,17 @@ my $stream_options = RtAudioStreamOptions->new(
     name  => 'LFO',
     flags => RTAUDIO_FLAGS_JACK_DONT_CONNECT,
 );
+
+my $slice = 1 / $samplerate;
+my $t = 0;
 sub lfo( $out, $in, $nframes, $stream_time, $stream_status, $userdata ) {
-    ...
+    my $buf;
+    for my $frame ( 1..$nframes ) {
+        $buf .= pack 'f', ( sin( pi2 * $t ) / 2 ) + 0.5;
+        $t += $slice;
+    }
+    my( $ptr, $size ) = scalar_to_buffer $buf;
+    memcpy( $out, $ptr, $size );
 }
 
 rtaudio_open_stream(
